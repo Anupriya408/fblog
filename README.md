c7c4c07b144a6c10d06dc8eef63b5607cc18039f
Anupriya408 <pihusinha0408@gmail.com>
2024-05-11 14:59:27 +0530

    validation done

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # fblog
+# screenshot
+
+
+
+Brief project description goes here...
+
+Screenshot
+
+![Screenshot](https://i.ibb.co/6XVswx5/screenshot.png)
+![Screenshot](https://ibb.co/CHQCJtS)
+
+
+## Installation
+
+Instructions for installation...
+
+## Usage
+
+Instructions for usage...
+
+## Contributing
+
+Guidelines for contributing...
+
+## License
+
+Specify the license information...
--- first_project/lib/main.dart
+++ first_project/lib/main.dart
@@ -17,7 +17,13 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
@@ -32,125 +38,145 @@ class LoginScreen extends StatelessWidget {
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(12.0),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                // Facebook logo
-                Image.asset(
-                  'images/facebook_image.jpg',
-                  height: 110,
-                  width: imageWidth,
-                ),
-                const SizedBox(height: 10.0),
-
-                Container(
-                  width: containerWidth,
-                  padding: const EdgeInsets.all(18),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.0),
-                    color: Colors.white,
-                    boxShadow: [
-                      const BoxShadow(
-                        // color: Colors.grey.withOpacity(0.8),
-                        // spreadRadius: 1,
-                        // blurRadius: 3,
-                        // offset: Offset(0, 1),
-                      ),
-                    ],
+          child: Form(
+            key: _formKey,
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  // Facebook logo
+                  Image.asset(
+                    'images/facebook_image.jpg',
+                    height: 110,
+                    width: imageWidth,
                   ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      const Text(
-                        'Login to Facebook',
-                        style: TextStyle(fontSize: 18),
-                      ),
-                      const SizedBox(height: 7),
-
-                      // Email TextField
-                      TextField(
-                        controller: _emailController,
-                        decoration: InputDecoration(
-                          labelText: 'Email address or phone number',
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(12.0),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(height: 16.0),
-
-                      // Password TextField
-                      TextField(
-                        controller: _passwordController,
-                        obscureText: true,
-                        decoration: InputDecoration(
-                          labelText: 'Password',
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(12.0),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(height: 16.0),
-
-                      // Login Button
-                      SizedBox(
-                        width: double.infinity,
-                        child: ElevatedButton(
-                          onPressed: () {},
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.blue,
-                          ),
-                          child: const Text(
-                            'Log In',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 18,
-                            ),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(height: 20.0),
-                  
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Expanded(
-                          child: Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                          child: TextButton(
-                          onPressed: () {},
-                          child: const Text(
-                          'Forgotten Account?',
-                           style: TextStyle(
-                           color: Colors.blue,
-                           fontSize: 14,
-                          ),
-                         ),
-                           ),
-                           ),
-                           ),
-                            Expanded(
-                           child: Padding(
-                              padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                             child: TextButton(
-                             onPressed: () {},
-                            child: const Text(
-                               'Sign up for Facebook',
-                             style: TextStyle(
-                              color: Colors.blue,
-                              fontSize: 14,
-                              ),
-                              ),
-                              ),
-                            ),
-                      ),
-                   ],
+                  const SizedBox(height: 10.0),
+                  Container(
+                    width: containerWidth,
+                    padding: const EdgeInsets.all(18),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.0),
+                      color: Colors.white,
+                      boxShadow: [
+                        const BoxShadow(),
+                      ],
                     ),
-                    ],
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        const Text(
+                          'Login to Facebook',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                        const SizedBox(height: 7),
+
+                        // Email TextFormField with validation here
+                        TextFormField(
+                          controller: _emailController,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter your email address...!!';
+                            }
+                          if (!value.endsWith('@gmail.com')) {
+                              return 'Please enter a valid email address..!!';
+                            }
+                            return null;
+                          },
+                          decoration: InputDecoration(
+                            labelText: 'Email address or phone number',
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(12.0),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 16.0),
+
+                        // Password TextFormField with validation here
+                        TextFormField(
+                          controller: _passwordController,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter your password here ...';
+                            }
+                            if (value.length < 6) {
+                              return 'Password must be at least 6 characters long';
+                            }
+                            return null;
+                          },
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            labelText: 'Password',
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(12.0),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 16.0),
+
+                        // Login Button
+                        SizedBox(
+                          width: double.infinity,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              if (_formKey.currentState?.validate() ?? false) {
+                                // Process login
+                              }
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.blue,
+                            ),
+                            child: const Text(
+                              'Log In',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 20.0),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Expanded(
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 8.0),
+                                child: TextButton(
+                                  onPressed: () {},
+                                  child: const Text(
+                                    'Forgotten Account?',
+                                    style: TextStyle(
+                                      color: Colors.blue,
+                                      fontSize: 14,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 8.0),
+                                child: TextButton(
+                                  onPressed: () {},
+                                  child: const Text(
+                                    'Sign up for Facebook',
+                                    style: TextStyle(
+                                      color: Colors.blue,
+                                      fontSize: 14,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
